d3c4793ee6199ac7607cb64d6a51dbbbb7cb9264
wishJinit <wishjinit@gmail.com>
2021-01-09 19:20:54 +0900

    feat : LiveEvent 클래스 추가

---
+++ app/src/main/java/com/nexters/giftarchiving/util/LiveEvent.kt
@@ -0,0 +1,47 @@
+package com.nexters.giftarchiving.util
+
+import android.util.Log
+import androidx.annotation.MainThread
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.Observer
+import java.util.concurrent.atomic.AtomicBoolean
+
+internal class LiveEvent<T> : MediatorLiveData<T>() {
+    private val mPending = AtomicBoolean(false)
+
+    @MainThread
+    override fun observe(owner: LifecycleOwner, observer: Observer<in T>) {
+        if (hasActiveObservers()) {
+            Log.w(TAG, "Multiple observers registered but only one will be notified of changes.")
+        }
+
+        // Observe the internal MutableLiveData
+        super.observe(
+            owner,
+            Observer { t ->
+                if (mPending.compareAndSet(true, false)) {
+                    observer.onChanged(t)
+                }
+            }
+        )
+    }
+
+    @MainThread
+    override fun setValue(t: T?) {
+        mPending.set(true)
+        super.setValue(t)
+    }
+
+    /**
+     * Used for cases where T is Void, to make calls cleaner.
+     */
+    @MainThread
+    fun call() {
+        value = null
+    }
+
+    companion object {
+        const val TAG = "LiveEvent"
+    }
+}
