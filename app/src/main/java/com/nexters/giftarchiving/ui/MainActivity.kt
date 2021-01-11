16fcb7dc648f05c6f08e3c25cb19df67a28b8bfd
wishJinit <wishjinit@gmail.com>
2021-01-11 20:01:40 +0900

    feat : MainActivity Base코드 연결 및 샘플 ui

---
+++ app/src/main/java/com/nexters/giftarchiving/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.nexters.giftarchiving.ui
+
+import com.nexters.giftarchiving.R
+import com.nexters.giftarchiving.base.BaseActivity
+import com.nexters.giftarchiving.databinding.ActivityMainBinding
+import com.nexters.giftarchiving.viewmodel.MainViewModel
+import org.koin.android.viewmodel.ext.android.viewModel
+
+internal class MainActivity : BaseActivity<MainViewModel, ActivityMainBinding>() {
+    override val layoutId = R.layout.activity_main
+    override val viewModel: MainViewModel by viewModel()
+}
