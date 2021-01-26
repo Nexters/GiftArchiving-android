d6061aec312a16f0e41a19086e4accf92c430577
wishJinit <wishjinit@gmail.com>
2021-01-26 23:49:01 +0900

    feat : background color 수정, font color 제거

--- app/src/main/java/com/nexters/giftarchiving/util/color/BackgroundColor.kt
+++ app/src/main/java/com/nexters/giftarchiving/util/color/BackgroundColor.kt
@@ -3,9 +3,9 @@ package com.nexters.giftarchiving.util.color
 import androidx.annotation.ColorRes
 import com.nexters.giftarchiving.R
 
-enum class BackgroundColor(@ColorRes val resId: Int) {
-    RED(R.color.colorRed),
-    GREEN(R.color.colorGreen),
-    BLUE(R.color.colorBlue),
-    WHITE(R.color.colorWhite)
+enum class BackgroundColor(@ColorRes val backgroundColor: Int, @ColorRes val fontColor: Int) {
+    MONO(R.color.colorMono, R.color.colorWhite),
+    BLUE(R.color.colorBlue, R.color.colorWhite),
+    ORANGE(R.color.colorOrange, R.color.colorWhite),
+    YELLOW(R.color.colorYellow, R.color.colorDarkGray)
 }
