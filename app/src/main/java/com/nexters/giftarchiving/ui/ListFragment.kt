a05a60a94580c2e50b227a7da827797c5b3156e6
JunhoSeo <tjwnsgh34@kau.kr>
2021-01-30 01:18:36 +0900

    ListFragment navargs 생성

--- app/src/main/java/com/nexters/giftarchiving/ui/ListFragment.kt
+++ app/src/main/java/com/nexters/giftarchiving/ui/ListFragment.kt
@@ -2,6 +2,7 @@ package com.nexters.giftarchiving.ui
 
 import android.os.Bundle
 import android.view.View
+import androidx.navigation.fragment.navArgs
 import com.google.android.material.tabs.TabLayoutMediator
 import com.nexters.giftarchiving.R
 import com.nexters.giftarchiving.base.BaseFragment
@@ -12,6 +13,7 @@ import org.koin.android.viewmodel.ext.android.viewModel
 internal class ListFragment : BaseFragment<ListViewModel, FragmentListBinding>() {
     override val layoutId = R.layout.fragment_list
     override val viewModel: ListViewModel by viewModel()
+    override val navArgs: ListFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
