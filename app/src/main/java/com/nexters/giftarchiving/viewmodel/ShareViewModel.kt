088931a5fc0ae890b3b72096955a68c83125be20
wishJinit <wishjinit@gmail.com>
2021-01-30 16:16:17 +0900

    feat : 완성(공유)화면 UI & 기능 추가 및 연결

--- app/src/main/java/com/nexters/giftarchiving/viewmodel/ShareViewModel.kt
+++ app/src/main/java/com/nexters/giftarchiving/viewmodel/ShareViewModel.kt
@@ -1,5 +1,23 @@
 package com.nexters.giftarchiving.viewmodel
 
+import android.graphics.Bitmap
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.viewModelScope
 import com.nexters.giftarchiving.base.BaseViewModel
+import com.nexters.giftarchiving.ui.ShareFragmentArgs
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
-internal class ShareViewModel : BaseViewModel()
+
+internal class ShareViewModel : BaseViewModel() {
+    val giftImg = MutableLiveData<Bitmap>()
+
+    init {
+        viewModelScope.launch {
+            navArgs<ShareFragmentArgs>()
+                .collect { giftImg.value = it.bitmap }
+        }
+    }
+
+    fun onClickSharedInstagramStory() {}
+}
