ee0fb85e1894761996042322c7c8bb58882eca96
wishJinit <wishjinit@gmail.com>
2021-01-30 16:07:15 +0900

    feat : background color theme 클래스 추가

--- app/src/main/java/com/nexters/giftarchiving/util/theme/BackgroundColorTheme.kt
+++ app/src/main/java/com/nexters/giftarchiving/util/theme/BackgroundColorTheme.kt
@@ -3,9 +3,9 @@ package com.nexters.giftarchiving.util.theme
 import androidx.annotation.ColorRes
 import com.nexters.giftarchiving.R
 
-enum class BackgroundColorTheme(@ColorRes val backgroundColor: Int, @ColorRes val fontColor: Int) {
-    MONO(R.color.colorMono, R.color.colorWhite),
-    BLUE(R.color.colorBlue, R.color.colorWhite),
-    ORANGE(R.color.colorOrange, R.color.colorWhite),
-    YELLOW(R.color.colorYellow, R.color.colorDarkGray)
+enum class BackgroundColorTheme(@ColorRes val backgroundColor: Int, val isDarkMode: Boolean) {
+    MONO(R.color.colorMono, true),
+    BLUE(R.color.colorBlue, true),
+    ORANGE(R.color.colorOrange, true),
+    YELLOW(R.color.colorYellow, false)
 }
