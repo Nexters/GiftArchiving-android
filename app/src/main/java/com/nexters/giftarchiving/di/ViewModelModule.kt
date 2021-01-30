6d8ae769847147a3aa53bf199ee152ad1467f018
wishJinit <wishjinit@gmail.com>
2021-01-30 16:28:24 +0900

    refactor : conflict 해결

--- app/src/main/java/com/nexters/giftarchiving/di/ViewModelModule.kt
+++ app/src/main/java/com/nexters/giftarchiving/di/ViewModelModule.kt
@@ -1,13 +1,13 @@
 package com.nexters.giftarchiving.di
 
 import com.nexters.giftarchiving.viewmodel.CropViewModel
-import com.nexters.giftarchiving.viewmodel.MainViewModel
-import com.nexters.giftarchiving.viewmodel.ShareViewModel
-import com.nexters.giftarchiving.viewmodel.WriteViewModel
 import com.nexters.giftarchiving.viewmodel.HomeViewModel
+import com.nexters.giftarchiving.viewmodel.ListViewModel
 import com.nexters.giftarchiving.viewmodel.LoginViewModel
 import com.nexters.giftarchiving.viewmodel.MainViewModel
+import com.nexters.giftarchiving.viewmodel.ShareViewModel
 import com.nexters.giftarchiving.viewmodel.SplashViewModel
+import com.nexters.giftarchiving.viewmodel.WriteViewModel
 import org.koin.dsl.module
 
 val viewModelModule = module {
