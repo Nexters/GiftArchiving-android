e9833dc6b20d35fba30af38019dd85ddd96cd66d
JunhoSeo <tjwnsgh34@kau.kr>
2021-02-07 17:09:25 +0900

    공지사항 추가

--- app/src/main/java/com/nexters/giftarchiving/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/nexters/giftarchiving/viewmodel/SettingsViewModel.kt
@@ -3,12 +3,17 @@ package com.nexters.giftarchiving.viewmodel
 import android.view.View
 import androidx.lifecycle.viewModelScope
 import com.nexters.giftarchiving.base.BaseViewModel
-import com.nexters.giftarchiving.ui.HomeFragmentDirections
+import com.nexters.giftarchiving.ui.SettingsFragmentDirections
 import com.nexters.giftarchiving.util.BackDirections
 import kotlinx.coroutines.launch
 
 internal class SettingsViewModel : BaseViewModel() {
     fun onClickBack() {
         navDirections.value = BackDirections()
+    }
+    fun onClickNotice(){
+        viewModelScope.launch {
+            navDirections.value= SettingsFragmentDirections.actionSettingFragmentToNoticeFragment()
+        }
     }
 }
