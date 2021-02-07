17c80bd22d18f0abd8331c86838e35d4091e59d0
JunhoSeo <tjwnsgh34@kau.kr>
2021-02-07 16:44:06 +0900

    back 버튼 추가

--- app/src/main/java/com/nexters/giftarchiving/viewmodel/SettingsViewModel.kt
+++ app/src/main/java/com/nexters/giftarchiving/viewmodel/SettingsViewModel.kt
@@ -4,9 +4,11 @@ import android.view.View
 import androidx.lifecycle.viewModelScope
 import com.nexters.giftarchiving.base.BaseViewModel
 import com.nexters.giftarchiving.ui.HomeFragmentDirections
+import com.nexters.giftarchiving.util.BackDirections
 import kotlinx.coroutines.launch
 
 internal class SettingsViewModel : BaseViewModel() {
-
-
+    fun onClickBack() {
+        navDirections.value = BackDirections()
+    }
 }
