0e7385f6fa9f8552965b7d70b9f8b3e53c045dc4
JunhoSeo <tjwnsgh34@kau.kr>
2021-02-08 01:40:59 +0900

    문의 메일 추가

--- app/src/main/java/com/nexters/giftarchiving/ui/SettingsFragment.kt
+++ app/src/main/java/com/nexters/giftarchiving/ui/SettingsFragment.kt
@@ -1,5 +1,6 @@
 package com.nexters.giftarchiving.ui
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import com.nexters.giftarchiving.R
@@ -14,6 +15,19 @@ internal class SettingsFragment : BaseFragment<SettingsViewModel, FragmentSettin
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.settingQuestion.setOnClickListener {
+            onClickEmail()
+        }
+    }
 
+    fun onClickEmail(){
+        val emailIntent = Intent(Intent.ACTION_SEND)
+        emailIntent.apply {
+            type = "text/html"
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("tjwnsgh34@gmail.com"))
+            putExtra(Intent.EXTRA_SUBJECT,"문의 및 건의하기")
+            putExtra(Intent.EXTRA_TEXT,"- 정확한 문의 파악을 위해 아래 정보를 작성해주세요!\n\n\n1. 문의 내용: \n\n2. 기프트집(카카오) 메일계정: \n\n★문의 관련 스크린샷을 첨부하시면\n보다 정확하고 빠른 확인이 가능합니다.")
+        }
+        startActivity(Intent.createChooser(emailIntent, "Send Email"))
     }
 }
