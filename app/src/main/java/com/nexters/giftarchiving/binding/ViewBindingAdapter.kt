d777466427ad67c510d86b4bcc9ca38ce79c24df
wishJinit <wishjinit@gmail.com>
2021-02-11 14:57:29 +0900

    feat : 카카오톡 메시지 공유 이미지 레이아웃 추가

--- app/src/main/java/com/nexters/giftarchiving/binding/ViewBindingAdapter.kt
+++ app/src/main/java/com/nexters/giftarchiving/binding/ViewBindingAdapter.kt
@@ -65,6 +65,11 @@ fun setBackgroundDrawable(v: View, @DrawableRes drawableRes: Int) {
     v.setBackgroundResource(drawableRes)
 }
 
+@BindingAdapter("android:backgroundTint")
+fun setBackgroundTintColor(v: View, colorTheme: BackgroundColorTheme) {
+    v.backgroundTintList = ContextCompat.getColorStateList(v.context, colorTheme.backgroundColor)
+}
+
 @BindingAdapter("popUpBackground")
 fun setPopUpBackgroundColor(v: View, colorTheme: BackgroundColorTheme?) {
     val colorByResource =
