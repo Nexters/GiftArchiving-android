f1a9a4dfb287f0734cba7b3c456b83d0635f356a
wishJinit <wishjinit@gmail.com>
2021-02-12 20:22:42 +0900

    fix : 로그인 request 타입 수정

--- app/src/main/java/com/nexters/giftarchiving/model/User.kt
+++ app/src/main/java/com/nexters/giftarchiving/model/User.kt
@@ -3,7 +3,7 @@ package com.nexters.giftarchiving.model
 import com.google.gson.annotations.SerializedName
 
 internal data class User(
-    @SerializedName("kakaoToken")
+    @SerializedName("token")
     val userId: String,
     @SerializedName("loginType")
     val loginType: String = "KAKAO"
