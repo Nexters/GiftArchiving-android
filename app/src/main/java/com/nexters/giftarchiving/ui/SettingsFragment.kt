9702ae2650c24f00593cdbaf6631fbd74b2c6eff
JunhoSeo <tjwnsgh34@kau.kr>
2021-02-19 02:05:16 +0900

    문의 이메일 수정

--- app/src/main/java/com/nexters/giftarchiving/ui/SettingsFragment.kt
+++ app/src/main/java/com/nexters/giftarchiving/ui/SettingsFragment.kt
@@ -24,7 +24,7 @@ internal class SettingsFragment : BaseFragment<SettingsViewModel, FragmentSettin
         val emailIntent = Intent(Intent.ACTION_SEND)
         emailIntent.apply {
             type = "text/html"
-            putExtra(Intent.EXTRA_EMAIL, arrayOf("tjwnsgh34@gmail.com"))
+            putExtra(Intent.EXTRA_EMAIL, arrayOf("giftzip.team@gmail.com"))
             putExtra(Intent.EXTRA_SUBJECT,"문의 및 건의하기")
             putExtra(Intent.EXTRA_TEXT,"- 정확한 문의 파악을 위해 아래 정보를 작성해주세요!\n\n\n1. 문의 내용: \n\n2. 기프트집(카카오) 메일계정: \n\n★문의 관련 스크린샷을 첨부하시면\n보다 정확하고 빠른 확인이 가능합니다.")
         }
