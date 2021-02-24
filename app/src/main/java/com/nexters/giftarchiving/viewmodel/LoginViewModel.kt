fe825437280db38604c7a009288e02173b1cdd9b
wishJinit <wishjinit@gmail.com>
2021-02-24 21:36:53 +0900

    feat : 로그인 대기 progress bar 추가

--- app/src/main/java/com/nexters/giftarchiving/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/nexters/giftarchiving/viewmodel/LoginViewModel.kt
@@ -1,5 +1,6 @@
 package com.nexters.giftarchiving.viewmodel
 
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import com.nexters.giftarchiving.base.BaseViewModel
 import com.nexters.giftarchiving.repository.LoginRepository
@@ -14,16 +15,19 @@ internal class LoginViewModel(
     private val preferenceRepository: PreferenceRepository
 ) : BaseViewModel() {
     val kakaoLogin = LiveEvent<Unit?>()
+    val isLoading = MutableLiveData<Boolean>()
 
     fun onClickKakaoLogin() {
         kakaoLogin.call()
     }
 
     fun signIn(userId: Long) {
+        isLoading.value = true
         viewModelScope.launch(Dispatchers.IO) {
             val response = loginRepository.signIn(userId = userId.toString())
             if (response.code() == SUCCESS_SIGN_UP_CODE || response.code() == SUCCESS_SIGN_IN_CODE) {
                 preferenceRepository.setUserId(userId)
+                isLoading.postValue(false)
                 navDirections.postValue(LoginFragmentDirections.actionLoginFragmentToHomeFragment())
             }
         }
