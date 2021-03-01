9bf391b6c1ef90ab201b5f836b6350d2f3bae0f3
wishJinit <wishjinit@gmail.com>
2021-03-02 03:05:50 +0900

    feat : 카카오링크 기록 확인 기능

--- app/src/main/java/com/nexters/giftarchiving/service/share/KakaoFeedMessage.kt
+++ app/src/main/java/com/nexters/giftarchiving/service/share/KakaoFeedMessage.kt
@@ -1,25 +1,34 @@
 package com.nexters.giftarchiving.service.share
 
+import com.kakao.sdk.template.model.Button
 import com.kakao.sdk.template.model.Content
 import com.kakao.sdk.template.model.FeedTemplate
 import com.kakao.sdk.template.model.Link
+import com.nexters.giftarchiving.util.KakaoParamKey
 
 internal object KakaoFeedMessage {
     private const val title = "\uD83C\uDF81기프트집 선물도착\uD83C\uDF81\n"
+    private const val buttonTitle = "구경하기"
 
-    fun getFeed(url: String, name: String, isReceive: Boolean): FeedTemplate {
+    fun getFeed(imageUrl: String, name: String, giftId: String, isReceive: Boolean): FeedTemplate {
         val description = when (isReceive) {
             true -> "${name}님이 나에게 보낸 선물이 도착했어요!"
             false -> "${name}님에게 보낸 선물이 도착했어요!"
         }
+        val buttonLinkParams = mapOf(KakaoParamKey.GIFT_ID to giftId)
         return FeedTemplate(
             content = Content(
                 title = title,
                 description = description,
-                imageUrl = url,
+                imageUrl = imageUrl,
                 link = Link()
             ),
-            buttonTitle = "구경하기"
+            buttons = listOf(
+                Button(
+                    buttonTitle,
+                    Link(androidExecParams = buttonLinkParams, iosExecParams = buttonLinkParams)
+                )
+            )
         )
     }
 }
---
+++ app/src/main/java/com/nexters/giftarchiving/util/KakaoParamKey.kt
@@ -0,0 +1,5 @@
+package com.nexters.giftarchiving.util
+
+object KakaoParamKey {
+    const val GIFT_ID = "gift_id"
+}
